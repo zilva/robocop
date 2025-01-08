c075e9217d1534ccbb5e044e5fa32ac0621b2cc1
Chamitha Silva <chamitha.silva@pronto.net>
2025-01-08 14:15:09 +0530

    added cutome event using GTM

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import './App.css';
 import Board from './Components/Board';
 
 const App = () => {
+
+  const handleClick = () => {
+    window.dataLayer.push({
+      event: 'custom_event_name', // The name of your event
+      eventCategory: 'user_action', // Optional: Custom parameters
+      eventAction: 'button_click',
+      eventLabel: 'Luffy clicked a button'
+    });
+  };
+
   return (
     <div className='app'>
       <Board />
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
